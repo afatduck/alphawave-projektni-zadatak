6b5d9016ff14507b1a9e5debecbab56964bd880c
Patrik <patrikmandic88@gmail.com>
2026-03-05 20:02:01 +0100

    Added render.com cofnig

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,11 +21,15 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
         FilamentAsset::register([
             Js::make("linechart", asset('js/linechart.js')),
             Css::make("linechart", asset("css/linechart.css"))
         ]);
+
+        if (env('APP_ENV') === 'production') {
+            $url->forceScheme('https');
+        }
     }
 }
